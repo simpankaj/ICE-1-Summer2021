8aed19c3d8cc976249e14086d2a3a127da1dfd76
Simple <patesimp@sheridancollege.ca>
2021-06-01 18:00:23 -0400

    name and id

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Name: Simple Patel
+ * Student ID: 991630305
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 import java.util.Random;
